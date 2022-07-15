ef77e66bb76101aab2d422e470a5da27f10a7e99
Akshat Jain <akshatjain384@gmail.com>
2022-07-16 00:34:28 +0530

    added calculator app and modifified

---
+++ Calculator/calci.js
@@ -0,0 +1,20 @@
+var a=8;
+var b=2;
+var c;
+function add(){
+    c=a+b;
+   document.getElementById("ans").innerHTML="The sum of given numbers is"+": "+c;
+}
+function sub(){
+     c=a-b;
+    document.getElementById("ans").innerHTML="The difference of given numbers is"+": "+c;
+ }
+ function div(){
+     c=a/b;
+    document.getElementById("ans").innerHTML="The division of given numbers is"+": "+c;
+ }
+ function mul(){
+     c=a*b;
+    document.getElementById("ans").innerHTML="The product of given numbers is"+": "+c;
+ }
+ 
