8c194c8b849fc448f6832a10ad7415e8aa39f26c
Akshat Jain <akshatjain384@gmail.com>
2022-07-17 11:29:47 +0530

    modified js file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function save()
     savev=savev+","+count;
     document.getElementById("show").innerHTML=savev;
     document.getElementById("count-el").innerHTML=0;
+    count=0;
 }
 function reset()
 {
